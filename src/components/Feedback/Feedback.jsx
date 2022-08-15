58595239a5280768cb6e6cda8d56625041c032e3
oLORDer <justsendmeow@gmail.com>
2022-08-15 15:36:14 +0300

    f1

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,7 +3,7 @@ import s from './feedback.module.scss';
 export default function Feedback() {
   return (
     <ul>
-      <li>hopP-22</li>
+      <li className={s.list}>hopP-22</li>
       <li>hopP-22</li>
       <li>hopP-22</li>
     </ul>
